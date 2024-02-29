cd777d64dc23f898e7f3bccc6d68db7a08cc8e66
nityamgoswami <goswaminityam9@gmail.com>
2024-02-28 20:12:07 -0500

    code checks if the number is divisible by 3 and returns "Fizz" if true.

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class FizzBuzz {
+
     public String fizzBuzz(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return null;
     }
 }
+
