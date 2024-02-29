809edb7d64e67c5d2dd3a18a1fdae4784fb74a83
nityamgoswami <goswaminityam9@gmail.com>
2024-02-28 20:20:59 -0500

    test 3 pass for multiple of 3 & 5

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -3,14 +3,16 @@ package org.example;
 public class FizzBuzz {
 
     public String fizzBuzz(int number) {
-        if (number % 3 == 0) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
             return "Fizz";
-        }
-        if (number % 5 == 0) {
+        } else if (number % 5 == 0) {
             return "Buzz";
         }
-
         return null;
     }
 }
+
+
 
